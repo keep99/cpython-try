83d87071445ca9b98c61776f9d5a533888b1d93f
root <root@localhost.localdomain>
2021-08-06 13:59:52 +0800

    README.md

--- Profile/test_json.py
+++ Profile/test_json.py
@@ -1,15 +1,33 @@
 import json
 import profile
 
-def fun():
-    data = [ { 'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5 } ]
+dataList = {}
+def make_data():
+    data = [ { 'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4.22, 'e' : "aaaa", "f" : [1,2,"444"]} ]
 
-    dataList = []
-    for i in range(1000000):
-        dataList.append(data)
+    
+    for i in xrange(1000000):
+        #dataList.append(data)
+        dataList[i] = data
 
-    data2 = json.dumps(data)
+
+def foo2():
+    data2 = json.dumps(dataList)
     # print(data2)
 
+def foo():
+    for i in xrange(100000000):
+        pass
+def foo2():
+    i = 0
+    while True:
+        if i > 100000000:
+            break
+        i += 1
 if __name__ == '__main__':
-    profile.run('fun()')
+    make_data()
+    #profile.run('foo()')
+    import time
+    import timeit
+    re =  timeit.repeat(foo2, repeat = 3, number  = 1)
+    print re, sum(re) /3
